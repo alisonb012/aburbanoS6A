5bc2d875836f3419b3bef8182990776941a4ad83
master <master@SMLAB306PC20_25>
2025-05-15 21:54:25 -0500

    Cambios

--- Models/Estudiante.cs
+++ Models/Estudiante.cs
@@ -9,10 +9,10 @@ namespace aburbanoS6A.Models
     public class Estudiante
     {
 
-            public int Codigo { get; set; }
+           
             public string Nombre { get; set; }
             public string Apellido { get; set; }
-            public int Edad { get; set; }
+            
 
         
     }
--- View/VCrud.xaml.cs
+++ View/VCrud.xaml.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using aburbanoS6A.Models;
+using Newtonsoft.Json;
 
 namespace aburbanoS6A.View;
 
